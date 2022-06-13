d85cde5e94678f272136a00c5b2a3a9426d6bd8c
x260 <x260@DESKTOP-SDD09U8>
2022-06-13 13:49:25 +0700

    Add project files.

---
+++ SekolahMVVM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SekolahMVVM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
